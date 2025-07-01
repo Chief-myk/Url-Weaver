92382acb3741f913705818246e0aca01dfdc7b0b
Chief-myk <mayankixa17@gmail.com>
2025-07-01 15:06:10 +0530

    changes

--- server/connenction.js
+++ server/connenction.js
@@ -1,34 +1,35 @@
-// const mongoose = require ("mongoose")
+const mongoose = require ("mongoose")
 
-// async function ConnectionDB(params) {
-//     return(
-//      await mongoose.connect(process.env.MONGODB_URI).then(()=>{
-//         console.log("Mongoose Connected Succesfully");
+async function ConnectionDB(params) {
+    return(
+    //  await mongoose.connect(process.env.MONGODB_URI).then(()=>{
+     await mongoose.connect("mongodb://localhost:27017/MyDatabases").then(()=>{
+        console.log("Mongoose Connected Succesfully");
         
-//         }).catch((error)=>{
-//         console.log("Something Went Wrong" ,error);
-//         })
+        }).catch((error)=>{
+        console.log("Something Went Wrong" ,error);
+        })
 
-//     )
-// }
-
-
-// module.exports = ConnectionDB
+    )
+}
 
 
-const mongoose = require('mongoose');
+module.exports = ConnectionDB
 
-const ConnectionDB = async () => {
-  try {
-    const URL = process.env.MONGODB_URI; // use env variable here
-    await mongoose.connect(URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected successfully.");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
-  }
-};
 
-module.exports = ConnectionDB;
+// const mongoose = require('mongoose');
+
+// const ConnectionDB = async () => {
+//   try {
+//     const URL = process.env.MONGODB_URI; // use env variable here
+//     await mongoose.connect(URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB connected successfully.");
+//   } catch (error) {
+//     console.error("MongoDB connection failed:", error);
+//   }
+// };
+
+// module.exports = ConnectionDB;
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
+
 app.use(cors())
 app.use(express.json());
 
@@ -22,10 +23,10 @@ app.use(express.static(path.join(__dirname, "dist")));
 // Use API routes
 app.use("/api", urlRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}, check: http://localhost:${port}/`);
+  console.log(`Example app listening on port ${port}, check now on http://localhost:${port}/`);
 });
